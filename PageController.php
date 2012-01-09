0f3cced9f984008c9713ad7f9a5807b4e56e9a13
Philipp Frischmuth <philipp@frischmuth24.de>
2012-01-09 14:34:55 +0100

    Added extracted version of page extension; Fixed a bug regarding window title

---
+++ PageController.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once 'OntoWiki/Controller/Component.php';
+
+/**
+ * Controller for OntoWiki
+ *
+ * @category   OntoWiki
+ * @package    OntoWiki_extensions_components_page
+ * @author     Sebastian Dietzold <dietzold@informatik.uni-leipzig.de>
+ * @author     Philipp Frischmuth <pfrischmuth@googlemail.com>
+ * @copyright  Copyright (c) 2008, {@link http://aksw.org AKSW}
+ * @license    http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
+ * @version    $Id$
+ */
+class PageController extends OntoWiki_Controller_Component
+{
+    /**
+     * Default action. Forwards to get action.
+     */
+    public function __call($action, $params)
+    {
+        OntoWiki_Navigation::disableNavigation();
+
+        $pagename = str_replace('Action', '', $action);
+        $pageTitles = $this->_privateConfig->titles->toArray();
+
+        if (isset($pageTitles[$pagename])) {
+            $this->view->placeholder('main.window.title')->set($this->_owApp->translate->_($pageTitles[$pagename]));
+        } else {
+            $this->view->placeholder('main.window.title')->set($this->_owApp->translate->_($pagename));
+        }
+
+        $this->render($pagename);
+    }
+}
